f581198827c84cbe897036fae183342a693a4cca
fowwo <54595444+fowwo@users.noreply.github.com>
2021-05-05 02:18:36 -0500

    Add buying and ignoring item functionality

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var activeScreen = document.getElementById("results");
+var cart = {
+	fushigi: -1,
+	tracy: -1,
+	nivea: -1,
+	frosted: -1,
+	butter: -1
+};
 
 /**
  * Switches the active screen.
@@ -16,5 +23,49 @@ function switchScreen(screen) {
 				activeScreen = e;
 			}, 50); // Allows for the screen to fade in
 		}, 100);
+	}
+}
+
+/**
+ * Handles all events when adding an item to the cart.
+ * @param {String} item - The id of the item.
+ */
+function buyItem(item) {
+	if (cart[item] !== undefined) {
+		let listing = document.getElementById(`${item}-listing`);
+		let add = document.getElementById(`${item}-add`);
+		let ignore = document.getElementById(`${item}-ignore`);
+		let panel = document.getElementById(`${item}-panel`);
+		listing.classList.add("bought");
+		listing.classList.remove("ignored");
+		add.classList.add("bought");
+		add.classList.remove("ignored");
+		ignore.classList.add("bought");
+		ignore.classList.remove("ignored");
+		panel.classList.add("bought");
+		panel.classList.remove("ignored");
+		cart[item] = 1;
+	}
+}
+
+/**
+ * Handles all events when ignoring an item.
+ * @param {String} item - The id of the item.
+ */
+function ignoreItem(item) {
+	if (cart[item] !== undefined) {
+		let listing = document.getElementById(`${item}-listing`);
+		let add = document.getElementById(`${item}-add`);
+		let ignore = document.getElementById(`${item}-ignore`);
+		let panel = document.getElementById(`${item}-panel`);
+		listing.classList.remove("bought");
+		listing.classList.add("ignored");
+		add.classList.remove("bought");
+		add.classList.add("ignored");
+		ignore.classList.remove("bought");
+		ignore.classList.add("ignored");
+		panel.classList.remove("bought");
+		panel.classList.add("ignored");
+		cart[item] = 0;
 	}
 }
